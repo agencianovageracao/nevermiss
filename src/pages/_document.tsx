f362f70986080da718d1a0d069163999dbd1aa57
AtomCodes <111660732+atomcodes@users.noreply.github.com>
2022-08-27 19:06:55 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,23 +21,19 @@ class MyDocument extends Document {
           <base href="/" />
           <meta name="robots" content="index,nofollow" />
           <meta name="author" content="AtomCodes (atomcodes#7973)" />
-          <meta name="copyright" content="AtomCodes" />
+          <meta name="copyright" content="NeverMiss" />
           <meta name="designer" content="AtomCodes (atomcodes#7973)" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta
-            name="keywords"
-            content="Atom Codes Web designer web developer front-end reactjs react angular javascript typescript nodejs apolloclient graphql graphcms"
-          />
-          <meta property="og:title" content="Atom Codes" />
+          <meta property="og:title" content="NeverMiss" />
           <meta
             property="og:description"
-            content="Nós somos a AtomCodes, um time de desenvolvimento focado em sites e sempre trazendo o que há de mais novo em tecnologias para os nossos clientes."
+            content="Reach the heights of cheating in games with the quality of Cheat.win! Look professional and always stay reliable."
           />
-          <meta property="og:url" content="https://atomcodes.com.br/" />
+          <meta property="og:url" content="https://nevermiss.pw/" />
           <meta property="og:type" content="website" />
           <meta property="og:locale" content="pt_BR" />
           <meta property="og:image" content="/brand/logo.png" />
@@ -45,21 +41,17 @@ class MyDocument extends Document {
           <meta property="twitter:card" content="summary" />
           <meta
             name="abstract"
-            content="Nós somos a AtomCodes, um time de desenvolvimento focado em sites e sempre trazendo o que há de mais novo em tecnologias para os nossos clientes."
+            content="Reach the heights of cheating in games with the quality of Cheat.win! Look professional and always stay reliable."
           />
           <meta
             name="description"
-            content="Nós somos a AtomCodes, um time de desenvolvimento focado em sites e sempre trazendo o que há de mais novo em tecnologias para os nossos clientes."
+            content="Reach the heights of cheating in games with the quality of Cheat.win! Look professional and always stay reliable."
           />
           <link rel="shortcut icon" href="/brand/logo.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://unpkg.com/aos@2.3.1/dist/aos.css"
             rel="stylesheet"
           />
         </Head>
