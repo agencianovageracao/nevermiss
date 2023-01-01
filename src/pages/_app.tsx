177d90c374aa0ca016c735dde1caea0f468b886c
Nova Geração <itslucas010@gmail.com>
2023-01-01 18:02:58 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import AOS from 'aos'
 
+import '../styles/globals.css'
 import 'aos/dist/aos.css'
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
-import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -16,6 +17,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-5415968318381293"
+        async={true}
+        onError={() => console.log('Adsense error')}
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
     </>
   )
 }
