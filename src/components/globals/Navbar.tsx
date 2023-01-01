a19eef0e70b242803e2f7ff44a2ca4efaabdec6a
Nova Geração <itslucas010@gmail.com>
2023-01-01 19:19:28 -0300

    Update Navbar.tsx

--- src/components/globals/Navbar.tsx
+++ src/components/globals/Navbar.tsx
@@ -158,7 +158,7 @@ export const Navbar: NextPage = () => {
                 className="text-lg flex items-center gap-4 py-3 px-5 bg-gray-800 text-white uppercase font-bold rounded-lg transition-all hover:bg-opacity-80 hover:scale-105"
                 onClick={() => router.push('https://forum.nevermiss.pw/')}
               >
-                <span>Fórum</span>
+                <span>Forum</span>
               </button>
             </div>
           </div>
