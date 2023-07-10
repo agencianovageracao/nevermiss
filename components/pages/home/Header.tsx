346f28c1bd99ee53587588710ff9e83a80620717
BRAGA <65302648+bragadz@users.noreply.github.com>
2023-07-10 21:30:00 +0000

    Update Header.tsx

--- components/pages/home/Header.tsx
+++ components/pages/home/Header.tsx
@@ -30,7 +30,11 @@ export const Header = () => {
               Cheats
             </Button>
            </Link>
-            <Button className="bg-gray-800 hover:brightness-125">Forum</Button>
+           <Link href="https://forum.nevermiss.pw/index.php?/forums/">
+            <Button className="bg-gray-800 hover:brightness-125"> 
+              Forum
+            </Button>
+           </Link>
           </div>
         </div>
         <div className="relative">
